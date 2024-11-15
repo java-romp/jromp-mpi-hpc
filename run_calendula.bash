9f1f770107e0f4be4b947beb0e080241308e911f
scastd <scastd00@gmail.com>
2024-11-15 15:53:16 +0100

    Remove mca parameter and use Infiniband by an `export`

--- run_calendula.bash
+++ run_calendula.bash
@@ -1,15 +1,28 @@
 #!/bin/bash
 
+function check_modules() {
+  local modules=("Java/21.0.2" "gradle_8.10.2")
+  local module
+
+  for module in "${modules[@]}"; do
+    if ! module is-loaded "$module"; then
+      echo "Module $module is not loaded. Please load it before running this script."
+      exit 1
+    fi
+  done
+}
+
 function main() {
-  local nodes=16                                  # Number of nodes
-  local n_tasks=32                                # Number of processes (MPI ranks)
-  local cpus_per_task=32                          # Threads that JROMP will use
-  local n_tasks_per_node=$((n_tasks / nodes))    # Number of processes per node
-  local base_node_name="cn6"                     # Base name of the nodes
-  local first_node=9                             # First node number. IMPORTANT NOTE: IceLake Rocky 8 nodes start at 9
-  local node_list=()                             # List of nodes
-  local node_list_str                            # List of nodes as a string separated by commas
+  local nodes=16                                     # Number of nodes
+  local n_tasks=32                                   # Number of processes (MPI ranks)
+  local cpus_per_task=32                             # Threads that JROMP will use
   local main_class="jromp.mpi.examples.FullParallel" # Main class to run
+
+  local n_tasks_per_node=$((n_tasks / nodes)) # Number of processes per node
+  local base_node_name="cn6"                  # Base name of the nodes
+  local first_node=9                          # First node number. IMPORTANT NOTE: IceLake Rocky 8 nodes start at 9
+  local node_list=()                          # List of nodes
+  local node_list_str                         # List of nodes as a string separated by commas
   local i
 
   # Create the list of nodes
@@ -27,6 +40,14 @@ function main() {
   # specified in the cpus_per_task variable
   export JROMP_NUM_THREADS=$cpus_per_task
 
+  echo "Running with the following parameters:"
+  echo "  Nodes: $nodes"
+  echo "  Processes: $n_tasks"
+  echo "  Threads per process: $cpus_per_task"
+  echo "  Processes per node: $n_tasks_per_node"
+  echo "  Node list: $node_list_str"
+  echo "  Main class: $main_class"
+
   # Run the slurm batch
   sbatch \
     --nodes=$nodes \
@@ -37,4 +58,5 @@ function main() {
     run.slurm $main_class
 }
 
+check_modules
 main "$@"
