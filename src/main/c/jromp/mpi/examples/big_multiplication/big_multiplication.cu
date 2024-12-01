7889e0196d356186c213449b410186e1da88da82
scastd <scastd00@gmail.com>
2024-12-01 18:37:21 +0100

    Moved CUDA includes to .cuh file and use cublasDgemm

--- src/main/c/jromp/mpi/examples/big_multiplication/big_multiplication.cu
+++ src/main/c/jromp/mpi/examples/big_multiplication/big_multiplication.cu
@@ -1,56 +1,47 @@
-#include <cstdio>
-#include <cstdlib>
-#include <cublas_v2.h>
-#include <cuda_runtime.h>
+#include "big_multiplication.cuh"
 
-#include "big_multiplication.h"
-
-void init_matrix(double *matrix, const size_t size) {
-    assert_non_null(matrix);
-
-    for (size_t i = 0; i < size; i++) {
-        matrix[i] = random_in_range(1, 1000);
-    }
+void cublas_multiplication(const size_t N, const double *d_A, const double *d_B, double *d_C, const double alpha,
+                           const double beta, const cublasHandle_t *handle) {
+    START_CUDA_TIMER(cublasDgemm)
+    CUBLAS_CALL(cublasDgemm(*handle, CUBLAS_OP_N, CUBLAS_OP_N, N, N, N, &alpha, d_A, N, d_B, N, &beta, d_C, N));
+    STOP_CUDA_TIMER(cublasDgemm)
+    std::cout << "Elapsed time (cublasDgemm): " << GET_CUDA_TIMER(cublasDgemm) << " ms" << std::endl;
 }
 
 int main(const int argc, char *argv[]) {
     if (argc != 2) {
-        printf("Usage: %s <N>\n", argv[0]);
+        std::cout << "Usage: " << argv[0] << " <N>" << std::endl;
     }
 
     const auto N = static_cast<size_t>(strtoll(argv[1], nullptr, 10));
+    std::cout << "Matrix size: " << N << std::endl;
 
     double *d_A, *d_B, *d_C;
     constexpr double alpha = 1.0;
     constexpr double beta = 0.0;
-    cublasHandle_t handle;
+    cublasHandle_t cublasH;
 
     auto *A = static_cast<double *>(malloc(N * N * sizeof(double)));
     auto *B = static_cast<double *>(malloc(N * N * sizeof(double)));
     auto *C = static_cast<double *>(malloc(N * N * sizeof(double)));
 
-    init_matrix(A, N * N);
-    init_matrix(B, N * N);
+    matrixInitialization(A, B, N);
 
     CUDA_CALL(cudaMalloc(&d_A, N * N * sizeof(double)));
     CUDA_CALL(cudaMalloc(&d_B, N * N * sizeof(double)));
     CUDA_CALL(cudaMalloc(&d_C, N * N * sizeof(double)));
 
-    CUBLAS_CALL(cublasCreate(&handle));
+    CUBLAS_CALL(cublasCreate(&cublasH));
 
     CUBLAS_CALL(cublasSetMatrix(N, N, sizeof(double), A, N, d_A, N));
     CUBLAS_CALL(cublasSetMatrix(N, N, sizeof(double), B, N, d_B, N));
     CUBLAS_CALL(cublasSetMatrix(N, N, sizeof(double), C, N, d_C, N));
 
-    START_CUDA_TIMER(gemm)
-    // CUBLAS_CALL(cublasDgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, M, K, &alpha, d_A, N, d_B, K, &beta, d_C, N));
-    CUBLAS_CALL(cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, N, N, &alpha, d_A, CUDA_R_16F, N, d_B, CUDA_R_16F, N,
-        &beta, d_C, CUDA_R_16F, N, CUDA_R_32F, CUBLAS_GEMM_DEFAULT));
-    STOP_CUDA_TIMER(gemm)
+    std::cout << "Start matrix multiplication" << std::endl;
+
+    cublas_multiplication(N, d_A, d_B, d_C, alpha, beta, &cublasH);
 
     CUBLAS_CALL(cublasGetMatrix(N, N, sizeof(double), d_C, N, C, N));
-
-    printf("Time: %f\n", GET_CUDA_TIMER(gemm));
 
     free(A);
     free(B);
@@ -58,7 +49,19 @@ int main(const int argc, char *argv[]) {
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
-    cublasDestroy(handle);
+    cublasDestroy(cublasH);
 
     return 0;
+}
+
+void matrixInitialization(double *a, double *b, const size_t n) {
+    assert_non_null(a);
+    assert_non_null(b);
+
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < n; j++) {
+            a[i * n + j] = randomInRange(1, 1000);
+            b[i * n + j] = randomInRange(1, 1000);
+        }
+    }
 }
--- src/main/c/jromp/mpi/examples/big_multiplication/big_multiplication.cuh
+++ src/main/c/jromp/mpi/examples/big_multiplication/big_multiplication.cuh
@@ -1,4 +1,69 @@
 #ifndef BIG_MULTIPLICATION_CUH
 #define BIG_MULTIPLICATION_CUH
 
+#include <assert.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cublas_v2.h>
+#include <cuda_runtime.h>
+#include <iostream>
+#include <time.h>
+
+#define assert_non_null(ptr) assert(ptr != NULL)
+#define UNUSED __attribute__((unused))
+
+#define START_CUDA_TIMER(name) \
+    cudaEvent_t name##_start, name##_end; \
+    cudaEventCreate(&name##_start); \
+    cudaEventCreate(&name##_end); \
+    cudaEventRecord(name##_start);
+
+#define STOP_CUDA_TIMER(name) \
+    cudaEventRecord(name##_end); \
+    cudaEventSynchronize(name##_end); \
+    float name##_elapsed; \
+    cudaEventElapsedTime(&name##_elapsed, name##_start, name##_end);
+
+#define GET_CUDA_TIMER(name) name##_elapsed
+
+#define CUDA_CALL(x)                                                                                                   \
+    {                                                                                                                  \
+        const cudaError_t a = (x);                                                                                     \
+        if (a != cudaSuccess) {                                                                                        \
+            std::cout << "\nCUDA Error: " << a << " at " << __FILE__ << ":" << __LINE__ << std::endl;                  \
+            exit(1);                                                                                                   \
+        }                                                                                                              \
+    }
+
+#define CUBLAS_CALL(x)                                                                                                 \
+    {                                                                                                                  \
+        cublasStatus_t stat = (x);                                                                                     \
+        if (stat != CUBLAS_STATUS_SUCCESS) {                                                                           \
+            std::cout << "\nCUBLAS Error: " << stat << " at " << __FILE__ << ":" << __LINE__ << std::endl;              \
+            exit(1);                                                                                                   \
+        }                                                                                                              \
+    }
+
+void matrixInitialization(double *a, double *b, size_t n);
+
+static int randomInRange(const int min, const int max) {
+    return min + random() % (max - min + 1);
+}
+
+/**
+ * Sets a secure random seed based on the current time.
+ *
+ * @see https://wiki.sei.cmu.edu/confluence/display/c/MSC32-C.+Properly+seed+pseudorandom+number+generators
+ */
+static void setRandomSeedSecure(const int rank) {
+    timespec ts;
+
+    if (timespec_get(&ts, TIME_UTC) == 0) {
+        perror("Error: timespec_get failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    srandom(ts.tv_nsec ^ ts.tv_sec ^ rank);
+}
+
 #endif // BIG_MULTIPLICATION_CUH
