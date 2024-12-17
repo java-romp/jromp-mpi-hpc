98867d1e13ff4609a6cd48e801bbf6fd7586d73a
scastd <scastd00@gmail.com>
2024-12-18 00:07:30 +0100

    Fixed number of nodes in configurations.bash

--- src/main/c/jromp/mpi/examples/gemm/configurations.bash
+++ src/main/c/jromp/mpi/examples/gemm/configurations.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function configurations {
-  local nodes_conf=(1 5 16)    # Incremented by one, because the master occupies one additional task, so one more node is needed
+  local nodes_conf=(1 8 16)    # Incremented by one, because the master occupies one additional task, so one more node is needed
   local n_tasks_conf=(2 16 31) # -1 for master, so 1, 15, 30 (divisors of the matrix size)
   local cpus_per_task_conf=(1 12 32)
   local optimization_level_conf=(0 1 2 3)
@@ -14,3 +14,5 @@ function configurations {
 
   echo " ****** Total combinations: $((${#nodes_conf[@]} * ${#n_tasks_conf[@]} * ${#cpus_per_task_conf[@]} * ${#optimization_level_conf[@]})) ******"
 }
+
+configurations
